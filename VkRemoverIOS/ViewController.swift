56b18d09eb63a4ce48e7c1304d65af8121a80c97
alexeykomov <alexeykofficial@gmail.com>
2019-10-27 13:21:32 +0300

    Initial Commit

---
+++ VkRemoverIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VkRemoverIOS
+//
+//  Created by Alex K on 10/27/19.
+//  Copyright © 2019 Alex K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
